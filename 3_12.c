b5ca2cc9095179a7d398573e5ad50b7a13bb9ff5
Gnanesh <gnaneshpingu@gmail.com>
2017-05-26 11:32:29 +0530

    duplicating file descriptors

---
+++ 3_12.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXSIZE 4096
+
+int main(void) {
+    int n, fdp_in, fdp_out;
+    char buf[MAXSIZE];
+
+    //dup actually duplicates file descriptor
+    //with dup2, you can choose your own fd number
+
+    fdp_in = dup(STDIN_FILENO);
+    fdp_out = dup2(STDOUT_FILENO, 7);
+
+    while ((n = read(fdp_in, buf, MAXSIZE)) > 0) {
+        if ((write(7, buf, n)) != n) { //can use fdp_out or 7
+            puts("write error");
+            return EXIT_FAILURE;
+        }
+        
+        if (n < 0) {
+            puts("read error");
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
