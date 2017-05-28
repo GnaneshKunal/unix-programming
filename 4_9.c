04f3c77e8d0ee9b1c0d4c50ab31a821ac25b5771
Gnanesh <gnaneshpingu@gmail.com>
2017-05-28 08:44:56 +0530

    umasking permisions of a file

---
+++ 4_9.c
@@ -0,0 +1,24 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define RWRWRW (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)
+
+int main(void) {
+    umask(0);
+    char buf[2048];
+    if (creat("foo", RWRWRW) < 0) {
+        strcpy(buf, "error for foo");
+        write(STDIN_FILENO, buf, strlen(buf));
+        exit(1);
+    }
+    umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (creat("bar", RWRWRW) < 0) {
+        strcpy(buf, "error for bar");
+        write(STDIN_FILENO, buf, strlen(buf));
+        exit(1);
+    }
+    exit(0);
+}
