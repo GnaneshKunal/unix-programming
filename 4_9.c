bff3c0fe249f22b38516c2d2885443c531743ee0
Gnanesh <gnaneshpingu@gmail.com>
2017-05-28 09:46:56 +0530

    umasking permisions of a file(*)

--- 4_9.c
+++ 4_9.c
@@ -11,13 +11,13 @@ int main(void) {
     char buf[2048];
     if (creat("foo", RWRWRW) < 0) {
         strcpy(buf, "error for foo");
-        write(STDIN_FILENO, buf, strlen(buf));
+        write(STDOUT_FILENO, buf, strlen(buf));
         exit(1);
     }
     umask(S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
     if (creat("bar", RWRWRW) < 0) {
         strcpy(buf, "error for bar");
-        write(STDIN_FILENO, buf, strlen(buf));
+        write(STDOUT_FILENO, buf, strlen(buf));
         exit(1);
     }
     exit(0);
