bd4085dbf0bf71f9c77e64f34765a84d8a78df26
Gnanesh <gnaneshpingu@gmail.com>
2017-05-28 09:00:59 +0530

    changing permission with chmod

---
+++ 4_12.c
@@ -0,0 +1,26 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int main(void) {
+    struct stat statbuf;
+    char *buf = malloc(200 * sizeof(char));
+    if (stat("foo", &statbuf) < 0) {
+        strcpy(buf, "stat error for foo");
+        write(STDIN_FILENO, buf, strlen(buf));
+        exit(1);
+    }
+    if (chmod("foo", (statbuf.st_mode & ~S_IXGRP) | S_ISGID) < 0) {
+        strcpy(buf, "chmod error for foo");
+        write(STDIN_FILENO, buf, strlen(buf));
+        exit(1);
+    }
+    if (chmod("bar", S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH) < 0) {
+        strcpy(buf, "chmod error for bar");
+        write(STDIN_FILENO, buf, strlen(buf));
+        exit(1);
+    }
+    exit(0);
+}
