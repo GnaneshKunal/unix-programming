d58f4f72c1b6bd3e7f75bc33a74786ea1f22f951
gnaneshkunal <gnaneshpingu@gmail.com>
2017-06-07 09:22:13 +0530

    incomplete implementation of sleep

---
+++ 10_7.c
@@ -0,0 +1,14 @@
+#include <signal.h>
+#include <unistd.h>
+
+static void sig_alrm(int signo) {
+    // just return to wake up the pause
+}
+
+unsigned int sleep1(unsigned int seconds) {
+    if (signal(SIGALRM, sig_alrm) == SIG_ERR)
+        return (seconds);
+    alarm(seconds); //start timer
+    pause(); // next caught signal wakes us up
+    return (alarm(0)); //turn off timer, return unslept time
+}
