45cb810f31e7a291c42f25221ef8b10389d51b5b
gnaneshkunal <gnaneshpingu@gmail.com>
2017-06-14 15:31:56 +0530

    `gather write` a file*

--- 14_22.c
+++ 14_22.c
@@ -4,29 +4,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-char buf1[3] = "abc", buf2[3] = "123";
+#define FILE_MODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH)
+
+char buf[][3] = { "abc", "123"};
 struct iovec iobuf[2];
 
 int main(void) {
     
     int fd, i, n;
 
-    if ((fd = open("temp.foo", O_RDWR |O_CREAT)) < 0) {
+    if ((fd = open("temp.foo", O_RDWR |O_CREAT|O_TRUNC, FILE_MODE)) < 0) {
         puts("open error");
         exit(1);
     }
 
-    iobuf[0].iov_base = buf1;
-    iobuf[1].iov_base = buf2;
-
     for (i = 0; i < 2; i++) {
+        iobuf[i].iov_base = buf +i;
         iobuf[i].iov_len = 3;
     }
-
     if ((n = writev(fd, iobuf, 2)) != 6) {
         puts("some thing went wrong");
         exit(1);
     }
+    close(fd);
     puts("write successful");
     exit(0);
 }
