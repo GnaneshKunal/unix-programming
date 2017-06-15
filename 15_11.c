0ead1feb37856acc6400c56331f0e964f985af59
gnaneshkunal <gnaneshpingu@gmail.com>
2017-06-15 13:50:07 +0530

    copy file to pager program using popen

---
+++ 15_11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PAGER "${PAGER:-more}" // environment variable by default
+#define MAXLINE 4096
+
+int main(int argc, char **argv) {
+    char line[MAXLINE];
+    FILE *fpin, *fpout;
+
+    if (argc != 2) {
+        puts("usage: a.out <pathname>");
+        exit(1);
+    }
+
+    if ((fpin = fopen(argv[1], "r")) == NULL ) {
+        printf("can't open %s\n", argv[1]);
+    }
+
+    if ((fpout = popen(PAGER, "w")) == NULL) {
+        puts("popen error");
+        exit(1);
+    }
+
+    while (fgets(line, MAXLINE, fpin) != NULL) {
+        if (fputs(line, fpout) == EOF) {
+            puts("fputs error to pipe");
+            exit(1);
+        }
+    } 
+
+    if (ferror(fpin)) {
+        puts("fgets error");
+        exit(1);
+    }
+
+    if (pclose(fpout) == -1) {
+        puts("pclose error");
+        exit(1);
+    }
+    exit(0);
+}
