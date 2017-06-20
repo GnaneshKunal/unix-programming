33c016cfee69da5d31ad23948196a327f2517984
gnaneshkunal <gnaneshpingu@gmail.com>
2017-06-20 16:04:43 +0530

    Connect with retry

---
+++ 16_10.c
@@ -0,0 +1,23 @@
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define MAXSLEEP 128
+
+int connect_retry(int sockfd, const struct sockaddr *addr, socklen_t alen) {
+    int numsec;
+
+    // Try to connect with exponential backoff
+
+    for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1) {
+        if (connect(sockfd, addr, alen) == 0) {
+            // connection accepted
+            return 0;
+        }
+
+        // delay before trying again
+
+        if (numsec <= MAXSLEEP / 2)
+            sleep(numsec);
+    }
+    return -1;
+}
