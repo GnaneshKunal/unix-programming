f0ea8c9a0f41c5f3538353683292cb3c487566b4
gnaneshkunal <gnaneshpingu@gmail.com>
2017-06-20 22:12:00 +0530

    Protable connect with retry

---
+++ 16_11.c
@@ -0,0 +1,28 @@
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define MAXSLEEP 128
+
+int connect_retry(int domain, int type, int protocol, const struct sockaddr *addr, socklen_t alen) {
+    int numsec, fd;
+
+    // Try to connect with exponential backoff
+
+    for (numsec = 1; numsec <= MAXSLEEP; numsec <<= 1) {
+        if ((fd = socket(domain, type, protocol)) < 0)
+            return -1;
+        if (connect(fd, addr, alen) == 0) {
+
+            // connect accepted
+
+            return fd;
+        }
+        close(fd);
+
+        // delay before trying again
+
+        if (numsec <= MAXSLEEP / 2)
+            sleep(numsec);
+    }
+    return -1;
+}
